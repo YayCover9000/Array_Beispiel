fd1e57df33d98c8d6f9830c2efac65c98675eb7f
YayCover9000 <yassuanf@googlemail.com>
2023-01-02 11:43:38 +0100

    initial commit and first start of code

---
+++ src/Array_Beispiel.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Array_Beispiel {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int laenge = sc.nextInt();
+        int[] array = new int[laenge + 1];
+        int[] aufruf1 = Array_Beispiel.teilbarkeit_durch_2(array);
+    }
+    public static int[] teilbarkeit_durch_2(int[] array) {
+        int[] arrayZwischen = new int[array.length];
+        for (int i = 0; i <= array.length; i++) {
+            arrayZwischen[i] = array[i];
+            if (arrayZwischen[i] % 2 == 0) {
+                System.out.println(arrayZwischen[i]);
+            }
+        }
+        return arrayZwischen;
+    }
+}
